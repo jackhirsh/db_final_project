eb4ca739b57b2cc26737fd87a0c330e41d237ccf
Alp Tutkun <tutkun.a@husky.neu.edu>
2019-11-21 15:30:31 -0500

    added create schema

--- sql/finaldbcreate.sql
+++ sql/finaldbcreate.sql
@@ -1,21 +1,65 @@
-drop database if exists climate;
-create database climate;
-create database climate;
-use climate;
-CREATE TABLE location (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+DROP DATABASE IF EXISTS climate;
+CREATE DATABASE climate;
+USE climate;
+CREATE TABLE location
+(
+    id        INT PRIMARY KEY AUTO_INCREMENT,
     longitude INT NOT NULL,
-    latitude INT NOT NULL,
+    latitude  INT NOT NULL,
     elevation INT NOT NULL
 );
 
-CREATE TABLE station (
-    id INT PRIMARY KEY,
+CREATE TABLE station
+(
+    id          INT PRIMARY KEY AUTO_INCREMENT,
     stationName VARCHAR(50) NOT NULL,
-    location INT NOT NULL,
-    CONSTRAINT fk FOREIGN KEY (location)
+    location    INT         NOT NULL,
+    CONSTRAINT FOREIGN KEY (location)
         REFERENCES location (id)
 );
+
+CREATE TABLE reading
+(
+    readID INT PRIMARY KEY AUTO_INCREMENT,
+    type   VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE wind
+(
+    id        INT PRIMARY KEY,
+    peakSpeed INT NOT NULL,
+    peakDir   INT NOT NULL,
+    sustSpeed INT NOT NULL,
+    sustDir   INT NOT NULL,
+    avgSpeed  INT NOT NULL,
+    CONSTRAINT FOREIGN KEY (id)
+        REFERENCES reading (readID)
+);
+
+CREATE TABLE temperature
+(
+    id  INT PRIMARY KEY,
+    max INT NOT NULL,
+    min INT NOT NULL,
+    avg INT NOT NULL,
+    CONSTRAINT FOREIGN KEY (id)
+        REFERENCES reading (readID)
+);
+
+CREATE TABLE precipitation
+(
+    id    INT PRIMARY KEY,
+    level INT NOT NULL,
+    CONSTRAINT FOREIGN KEY (id)
+        REFERENCES reading (readID)
+);
+
+CREATE TABLE time
+(
+    timeID   INT PRIMARY KEY,
+    timeType VARCHAR(10) NOT NULL,
+    value    DATE        NOT NULL
+)
 
 
 
